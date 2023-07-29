03e8759d936fe4fa0441852bd7d63d470c5b16c6
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-07-29 14:52:44 +0100

    Update init.sql

--- mysql-dev/init.sql
+++ mysql-dev/init.sql
@@ -59,6 +59,11 @@ CREATE TABLE problems (
     author_user_id BIGINT UNSIGNED NOT NULL,
     name TEXT NOT NULL,
     statement TEXT NOT NULL,
+    input_statement TEXT NOT NULL,
+    output_statement TEXT NOT NULL,
+    notes TEXT NOT NULL,
+    time_restriction TINYINT UNSIGNED NOT NULL,
+    memory_restriction INT UNSIGNED NOT NULL,
     private BOOLEAN NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (author_user_id) REFERENCES users(id)
@@ -78,8 +83,7 @@ CREATE TABLE test_cases (
     problem_id BIGINT UNSIGNED NOT NULL,
     input TEXT NOT NULL,
     solution TEXT NOT NULL,
-    time_restriction TINYINT UNSIGNED NOT NULL,
-    memory_restriction INT UNSIGNED NOT NULL,
+    score INT UNSIGNED NOT NULL,
     opened BOOLEAN NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (problem_id) REFERENCES problems(id)
@@ -117,23 +121,37 @@ CREATE TABLE competition_submissions (
     FOREIGN KEY (team_id) REFERENCES teams(id)
 );
 
+CREATE TABLE verdicts (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    text TEXT NOT NULL,
+    PRIMARY KEY (id)
+)
+
 CREATE TABLE submission_results (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     submission_id BIGINT UNSIGNED NOT NULL,
     test_case_id BIGINT UNSIGNED NOT NULL,
-    error TEXT NOT NULL,
-    output TEXT NOT NULL,
-    correct BOOLEAN NOT NULL,
+    verdict_id BIGINT UNSIGNED NOT NULL,
+    verdict_details TEXT NOT NULL,
     time_taken TINYINT UNSIGNED NOT NULL,
     memory_taken INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (submission_id) REFERENCES submissions(id),
-    FOREIGN KEY (test_case_id) REFERENCES test_cases(id)
+    FOREIGN KEY (test_case_id) REFERENCES test_cases(id),
+    FOREIGN KEY (verdict_id) REFERENCES verdicts(id)
 );
 
 INSERT INTO languages (name, version, supported) VALUES ('Python 3', '3.10', 1);
 INSERT INTO languages (name, version, supported) VALUES ('C++ 17', 'g++ 11.2', 1);
 INSERT INTO languages (name, version, supported) VALUES ('C 17', 'gcc 11.2', 1);
+
+INSERT INTO verdicts (text) VALUES ('Correct answer');
+INSERT INTO verdicts (text) VALUES ('Wrong answer');
+INSERT INTO verdicts (text) VALUES ('Compilation error');
+INSERT INTO verdicts (text) VALUES ('Runtime error');
+INSERT INTO verdicts (text) VALUES ('Time limit');
+INSERT INTO verdicts (text) VALUES ('Memory limit');
+INSERT INTO verdicts (text) VALUES ('Internal error');
 
 INSERT INTO users (username, email, name, password) VALUES ('admin', 'admin@admin', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 INSERT INTO teams (name, owner_user_id, active, individual) VALUES ('admin', 1, 1, 1);
